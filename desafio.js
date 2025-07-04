fe73453f660b8fcd1dc1b20bd349b0cb1827b6b4
Lindissy Almeida <lindissythais@gmail.com>
2025-07-04 20:48:53 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,11 @@
+let listaLigugemProgramacao = ['JavaScript','C','C++', 'Kotlin','Python'];
+
+// console.log (listaLigugemProgramacao)
+
+listaLigugemProgramacao.push('Java', 'Ruby', 'GoLang')
+
+// console.log(listaLigugemProgramacao)
+
+let listaNomes = ['Lorena', 'Antonia', 'Felicia'];
+
+console.log(listaNomes[3])
